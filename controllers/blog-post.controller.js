429cab8ab5101020b94a039800b2dfc1a5858106
coreyc <ctc272@nyu.edu>
2018-10-23 07:25:12 -0500

    added test structure and setup

--- controllers/blog-post.controller.js
+++ controllers/blog-post.controller.js
@@ -9,6 +9,8 @@ const postBlogpost = async (req, res, next) => {
   const {user, content} = req.body
   try {
     await createBlogpost(user, content)
+    // other service call (or same service, different function can go here)
+    // i.e. - await generateBlogpostPreview()
     res.sendStatus(201)
     next()
   } catch(e) {
